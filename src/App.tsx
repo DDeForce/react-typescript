1362f47307e6f2f2d7ee6f1c6535fcc3b1388480
Martin <stamnitram@gmail.com>
2022-06-03 01:35:37 +0200

    models ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
   return (
     <div>
-      <Todos items={["learn React", "learn Typescript"]} />
+      <Todos items={todos} />
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 
-const Todos: FC<{ items: string[] }> = (props) => {
+import Todo from "../models/todo";
+
+const Todos: FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
